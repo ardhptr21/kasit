d86c87fe0b76b8fd2e72d2f400343057221dfbe4
ardhptr21 <ardhiputrapradana21@gmail.com>
2024-09-04 22:30:54 +0700

    feat: initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex justify-center items-center flex-col">
+      <h1 className="font-bold text-3xl">KasIT</h1>
+      <p>Kasnya anak IT bjirrrr....</p>
+    </main>
+  );
+}
